58b240f7efedf1b76f5b836fec5cc35470824381
Aleksandr Karpenko <25748152+mekuto@users.noreply.github.com>
2026-01-21 21:03:10 +0300

    fix: реализация шаблона Builder (Строитель) переработана.

--- homework_3/src/etc/Builder.java
+++ homework_3/src/etc/Builder.java
@@ -2,7 +2,7 @@
  * File: Builder.java
  * Path: ./etc/
  *
- * Date: 2026-01-12
+ * Date: 2026-01-21
  * Author: Aleksandr Karpenko Ivanovich
  *
  * Thesis: Builder template
@@ -10,215 +10,238 @@
 
 package etc;
 
-import java.time.LocalDate;
-
 class Car {
-    private String idBrand;
-    private String brand;
-    private String brandCyrillic;
-    private int fame;
-    private String country;
-    private int startYearOfBrand;
-    private int endYearOfBrand;
-    private String idModel;
-    private String model;
-    private String modelCyrillic;
-    private String modelClass;
-    private int startYearOfModel;
-    private int endYearOfModel;
+    String idBrand = "";
+    String brand = "";
+    String brandCyrillic = "";
+    int fame = 0;
+    String country = "";
+    int startYearOfBrand = -1;
+    int endYearOfBrand = -1;
+    String idModel = "";
+    String model = "";
+    String modelCyrillic = "";
+    String modelClass = "";
+    int startYearOfModel = -1;
+    int endYearOfModel = -1;
+}
 
-    public Car() {
-        idBrand = "";
-        brand = "";
-        brandCyrillic = "";
-        fame = 0;
-        country = "";
-        idModel = "";
-        model = "";
-        modelCyrillic = "";
-        modelClass = "";
+abstract class  CarBuilderConcept {
+    protected Car targetCar;
+
+    public Car getCar() {
+        return targetCar;
     }
 
-    public Car(String brand, String model) throws NullPointerException {
-        this();
-        if (brand == null || model == null) throw new NullPointerException("class Car(" + brand + ", " + model +")");
-        this.brand = brand;
-        this.model = model;
+    public void createNew() {
+        targetCar = new Car();
     }
 
-    public String getIdBrand() {
-        return idBrand;
-    }
+    public abstract void setBrand();
+    public abstract void setFame();
+    public abstract void setCountry();
+    public abstract void setModel();
+    public abstract void setModelClass();
+}
 
-    public String getBrand() {
-        return brand;
-    }
-
-    public String getBrandCyrillic() {
-        return brandCyrillic;
-    }
-
-    public int getFame() {
-        return fame;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public int getStartYearOfBrand() {
-        return startYearOfBrand;
-    }
-
-    public int getEndYearOfBrand() {
-        return endYearOfBrand;
-    }
-
-    public String getIdModel() {
-        return idModel;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public String getModelCyrillic() {
-        return modelCyrillic;
-    }
-
-    public String getModelClass() {
-        return modelClass;
-    }
-
-    public int getStartYearOfModel() {
-        return startYearOfModel;
-    }
-
-    public int getEndYearOfModel() {
-        return endYearOfModel;
-    }
-
-    public Car setIdBrand(String idBrand) throws NullPointerException {
-        if (idBrand == null) throw new NullPointerException("class Car: setIdMark(null)");
-        this.idBrand = idBrand;
-        return this;
-    }
-
-    public Car setBrand(String brand) throws NullPointerException {
-        if (brand == null) throw new NullPointerException("class Car: setMark(null)");
-        this.brand = brand;
-        return this;
-    }
-
-    public Car setBrandCyrillic(String brandCyrillic) throws NullPointerException {
-        if (brandCyrillic == null) throw new NullPointerException("class Car: setMarkCyrillic(null)");
-        this.brandCyrillic = brandCyrillic;
-        return this;
-    }
-
-    public Car setFame(int fame) {
-        if (fame != 0 && fame != 1 ) return this;
-        this.fame = fame;
-        return this;
-    }
-
-    public Car setCountry(String country) throws  NullPointerException {
-        if (country == null) throw new NullPointerException("class Car: setCountry(null)");
-        this.country = country;
-        return this;
-    }
-
-    public Car setStartYearOfBrand(int startYearOfBrand) {
-        if (startYearOfBrand < 1799 || startYearOfBrand > LocalDate.now().getYear()) return this;
-        this.startYearOfBrand = startYearOfBrand;
-        return this;
-    }
-
-    public Car setEndYearOfBrand(int endYearOfBrand) {
-        if (endYearOfBrand < 1799 || endYearOfBrand > LocalDate.now().getYear()) return this;
-        this.endYearOfBrand = endYearOfBrand;
-        return this;
-    }
-
-    public Car setIdModel(String idModel) throws NullPointerException {
-        if (idModel == null) throw new NullPointerException("class Car: setIdModel(null)");
-        this.idModel = idModel;
-        return this;
-    }
-
-    public Car setModel(String model) throws NullPointerException {
-        if (model == null) throw new NullPointerException("class Car: setModel(null)");
-        this.model = model;
-        return this;
-    }
-
-    public Car setModelCyrillic(String modelCyrillic) throws NullPointerException {
-        if (modelCyrillic == null) throw new NullPointerException("class Car: setModelCyrillic(null)");
-        this.modelCyrillic = modelCyrillic;
-        return this;
-    }
-
-    public Car setModelClass(String modelClass) throws NullPointerException {
-        if (modelClass == null) throw new NullPointerException("class Car: setModelClass(null)");
-        this.modelClass = modelClass;
-        return this;
-    }
-
-    public Car setStartYearOfModel(int startYearOfModel) {
-        if (startYearOfModel < 1799 || startYearOfModel > LocalDate.now().getYear()) return this;
-        this.startYearOfModel = startYearOfModel;
-        return this;
-    }
-
-    public Car setEndYearOfModel(int endYearOfModel) {
-        if (endYearOfModel < 1799 || endYearOfModel > LocalDate.now().getYear()) return this;
-        this.endYearOfModel = endYearOfModel;
-        return this;
+class BuildCarAcuraCDX extends CarBuilderConcept {
+//"ACURA,Acura,Акура,0,Япония,1986,2026,ACURA_CDX,CDX,сдх,D,2016,2021"
+    @Override
+    public void setBrand() {
+        targetCar.idBrand = "ACURA";
+        targetCar.brand = "Acura";
+        targetCar.brandCyrillic = "Акура";
+        targetCar.startYearOfBrand = 1986;
+        targetCar.endYearOfModel = 2026;
     }
 
     @Override
-    public String toString() {
-        String fameString = (fame == 0) ? "False" : "True";
-        return "ID Brand:\t\t\t" + idBrand + "\n" +
-                "Brand:\t\t\t\t" + brand + "\n" +
-                "Brand (Cyrillic):\t" + brandCyrillic + "\n" +
-                "Fame:\t\t\t\t" + fameString + "\n" +
-                "Country:\t\t\t" + country + "\n" +
-                "Brand from year:\t" + startYearOfBrand + "\n" +
-                "Brand to year:\t\t" + endYearOfBrand + "\n" +
-                "ID Model:\t\t\t" + idModel + "\n" +
-                "Model:\t\t\t\t" + model + "\n" +
-                "Model (Cyrillic):\t" + modelCyrillic + "\n" +
-                "Model class:\t\t" + modelClass + "\n" +
-                "Model from year:\t" + startYearOfModel + "\n" +
-                "Model end year:\t\t" + endYearOfModel + "\n";
+    public void setFame() {
+        targetCar.fame = 0;
+    }
+
+    @Override
+    public void setCountry() {
+        targetCar.country = "Япония";
+    }
+
+    @Override
+    public void setModel() {
+        targetCar.idModel = "ACURA_CDX";
+        targetCar.model = "CDX";
+        targetCar.modelCyrillic = "сдх";
+        targetCar.startYearOfModel = 2016;
+        targetCar.endYearOfModel = 2021;
+    }
+
+    @Override
+    public void setModelClass() {
+        targetCar.modelClass = "D";
+    }
+}
+
+class BuildCarAbarth124Spider extends CarBuilderConcept {
+// ABARTH,Abarth,Абарт,0,Италия,2008,2026,ABARTH_124_SPIDER,124 Spider,124 спайдер,S,2016,2020
+
+    @Override
+    public void setBrand() {
+        targetCar.idModel = "ABARTH";
+        targetCar.model = "Abarth";
+        targetCar.modelCyrillic = "Абарт";
+        targetCar.startYearOfBrand = 2008;
+        targetCar.endYearOfBrand = 2026;
+    }
+
+    @Override
+    public void setFame() {
+        targetCar.fame = 0;
+    }
+
+    @Override
+    public void setCountry() {
+        targetCar.country = "Италия";
+    }
+
+    @Override
+    public void setModel() {
+        targetCar.idModel = "ABARTH_124_SPIDER";
+        targetCar.model = "124 Spider";
+        targetCar.modelCyrillic = "124 спайдер";
+        targetCar.startYearOfModel = 2016;
+        targetCar.endYearOfModel = 2020;
+    }
+
+    @Override
+    public void setModelClass() {
+        targetCar.modelClass = "S";
+    }
+}
+
+class BuildCarAudiA4 extends CarBuilderConcept {
+// AUDI,Audi,Ауди,1,Германия,1927,2026,AUDI_A4,A4,А4,D,1994,2026
+
+    @Override
+    public void setBrand() {
+        targetCar.idBrand = "AUDI";
+        targetCar.brand = "Audi";
+        targetCar.brandCyrillic = "Ауди";
+        targetCar.startYearOfBrand = 1917;
+        targetCar.endYearOfBrand = 2026;
+    }
+
+    @Override
+    public void setFame() {
+        targetCar.fame = 1;
+    }
+
+    @Override
+    public void setCountry() {
+        targetCar.country = "Германия";
+    }
+
+    @Override
+    public void setModel() {
+        targetCar.idModel = "AUDI_A4";
+        targetCar.model = "A4";
+        targetCar.modelCyrillic = "А4";
+        targetCar.startYearOfModel = 1994;
+        targetCar.endYearOfModel = 2026;
+    }
+
+    @Override
+    public void setModelClass() {
+        targetCar.modelClass = "D";
+    }
+}
+
+class BuildCarToyota4Runner extends CarBuilderConcept {
+// TOYOTA,Toyota,Тойота,1,Япония,1960,2026,TOYOTA_4RUNNER,4Runner,Фораннер,J,1984,2026
+
+    @Override
+    public void setBrand() {
+        targetCar.idBrand = "TOYOTA";
+        targetCar.brand = "Toyota";
+        targetCar.brandCyrillic = "Тойота";
+        targetCar.startYearOfBrand = 1960;
+        targetCar.endYearOfBrand = 2026;
+    }
+
+    @Override
+    public void setFame() {
+        targetCar.fame = 1;
+    }
+
+    @Override
+    public void setCountry() {
+        targetCar.country = "Япония";
+    }
+
+    @Override
+    public void setModel() {
+        targetCar.idModel = "TOYOTA_4RUNNER";
+        targetCar.model = "4Runner";
+        targetCar.modelCyrillic = "Фораннер";
+        targetCar.startYearOfModel = 1984;
+        targetCar.endYearOfModel = 2026;
+    }
+
+    @Override
+    public void setModelClass() {
+        targetCar.modelClass = "J";
+    }
+}
+
+class BuildDirector {
+    private CarBuilderConcept selectedBuilder;
+
+    public void setCarBuilder(CarBuilderConcept choice) {
+        selectedBuilder = choice;
+    }
+
+    public Car getCar() {
+        return selectedBuilder.getCar();
+    }
+
+    public void constructionCar() {
+        selectedBuilder.createNew();
+        selectedBuilder.setBrand();
+        selectedBuilder.setFame();
+        selectedBuilder.setCountry();
+        selectedBuilder.setModel();
+        selectedBuilder.setModelClass();
     }
 }
 
 public class Builder {
     private final static String MODULE_INFO =
-            "This module not for production use. Only for studying purpose.";
+        "This module not for production use. Only for studying purpose.";
 
     public static void main(String[] args) {
-        String carRecord = "ACURA,Acura,Акура,0,Япония,1986,2025,ACURA_CDX,CDX,сдх,D,2016,2021";
-        var carField = carRecord.split(",");
+        BuildDirector buildDirector = new BuildDirector();
+        CarBuilderConcept toyota4Runner = new BuildCarToyota4Runner();
 
-        Car yourCar;
+        buildDirector.setCarBuilder(toyota4Runner);
+        buildDirector.constructionCar();
 
-        yourCar = new Car(carField[ 1 ], carField[ 8 ])
-                .setIdBrand(carField[ 0 ])
-                .setBrandCyrillic(carField[ 2 ])
-                .setFame(Integer.parseInt(carField[ 3 ]))
-                .setCountry(carField[ 4 ])
-                .setStartYearOfBrand(Integer.parseInt(carField[ 5 ]))
-                .setEndYearOfBrand(Integer.parseInt(carField[ 6 ]))
-                .setIdModel(carField[ 7 ])
-                .setModel(carField[ 8 ])
-                .setModelCyrillic(carField[ 9 ])
-                .setModelClass(carField[ 10 ])
-                .setStartYearOfModel(Integer.parseInt(carField[ 11 ]))
-                .setEndYearOfModel(Integer.parseInt(carField[ 12 ]));
+        Car newToyotaCar = buildDirector.getCar();
 
-        System.out.println(yourCar);
+        System.out.println(formatingStringForCar(newToyotaCar));
+    }
+
+    private static String formatingStringForCar(Car car) {
+        String fameString = (car.fame == 0) ? "False" : "True";
+        return  "ID Brand:\t\t\t" + car.idBrand + "\n" +
+                "Brand:\t\t\t\t" + car.brand + "\n" +
+                "Brand (Cyrillic):\t" + car.brandCyrillic + "\n" +
+                "Fame:\t\t\t\t" + fameString + "\n" +
+                "Country:\t\t\t" + car.country + "\n" +
+                "Brand from year:\t" + car.startYearOfBrand + "\n" +
+                "Brand to year:\t\t" + car.endYearOfBrand + "\n" +
+                "ID Model:\t\t\t" + car.idModel + "\n" +
+                "Model:\t\t\t\t" + car.model + "\n" +
+                "Model (Cyrillic):\t" + car.modelCyrillic + "\n" +
+                "Model class:\t\t" + car.modelClass + "\n" +
+                "Model from year:\t" + car.startYearOfModel + "\n" +
+                "Model end year:\t\t" + car.endYearOfModel + "\n";
     }
 }
